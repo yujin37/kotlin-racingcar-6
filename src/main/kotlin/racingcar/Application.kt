10b2164b2d4a338a9e5c4cc6b4b139d1f36a0899
yujin37 <yj061628@gmail.com>
2023-10-29 14:08:08 +0900

    refactor(자동차 경주 게임): 테스트 코드 작성을 위한 코드 이동
    - 메인에 존재하던 자동차 이름 입력 부분 함수 생성
    - 메인에 존재하던 시도 횟수 입력 함수 생성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,15 +8,23 @@ class RacingCarValidator {
             throw IllegalArgumentException()
         }
     }
+    fun inputCars(): List<String> {
+        val cars = Console.readLine()
+        val racingCars = cars.split(',')
+        for(racingCar in racingCars){
+            RacingCarValidator().validateCarName(racingCar)
+        }
+        return racingCars
+    }
+    fun trying(): Int {
+        println("시도 할 횟수는 몇 회인가요?")
+        val trying = Console.readLine()
+        return trying.toInt()
+    }
 }
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
-    val cars = Console.readLine()
-    val racingCars = cars.split(',')
-    for(racingCar in racingCars){
-        RacingCarValidator().validateCarName(racingCar)
-    }
-    println("시도 할 횟수는 몇 회인가요?")
-    val trying = Console.readLine()
-    RacingGames().checkTime(racingCars, trying.toInt())
+    val racingCars = RacingCarValidator().inputCars()
+    val trying = RacingCarValidator().trying()
+    RacingGames().checkTime(racingCars, trying)
 }
