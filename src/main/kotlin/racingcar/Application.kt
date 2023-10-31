f737928b67d6eae559dbd88fa5120916e4754b03
yujin37 <yj061628@gmail.com>
2023-11-01 02:39:17 +0900

    fix(자동차 경주 게임 테스트): 횟수 입력 출력 메시지 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -26,7 +26,7 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val tempCars = Console.readLine()
     val racingCars = RacingValidator().inputCars(tempCars)
-    println("시도 할 횟수는 몇 회인가요?")
+    println("시도할 횟수는 몇 회인가요?")
     val times = Console.readLine()
     val trying = RacingValidator().trying(times)
     RacingGames().checkTime(racingCars, trying)
